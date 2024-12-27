a5915eaf4f5521a206f9d71fc339142420686d23
Benedikt Fein <fein@fim.uni-passau.de>
2024-12-27 10:31:56 +0100

    chore: replace poetry with uv

---
+++ flake.nix
@@ -0,0 +1,51 @@
+# SPDX-FileCopyrightText: 2024 Benedikt Fein
+#
+# SPDX-License-Identifier: EUPL-1.2
+
+{
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    systems.url = "github:nix-systems/default";
+    devenv.url = "github:cachix/devenv";
+    devenv.inputs.nixpkgs.follows = "nixpkgs";
+  };
+
+  nixConfig = {
+    extra-trusted-public-keys = "devenv.cachix.org-1:w1cLUi8dv3hnoSPGAuibQv+f9TZLr6cv/Hm9XgU50cw=";
+    extra-substituters = "https://devenv.cachix.org";
+  };
+
+  outputs = {
+    self,
+    nixpkgs,
+    devenv,
+    systems,
+    ...
+  } @ inputs: let
+    forEachSystem = nixpkgs.lib.genAttrs (import systems);
+  in {
+    packages = forEachSystem (system: {
+      devenv-up = self.devShells.${system}.default.config.procfileScript;
+      devenv-test = self.devShells.${system}.default.config.test;
+    });
+
+    devShells =
+      forEachSystem
+      (system: let
+        pkgs = nixpkgs.legacyPackages.${system};
+      in {
+        default = devenv.lib.mkShell {
+          inherit inputs pkgs;
+          modules = [
+            {
+              languages.python = {
+                enable = true;
+                package = pkgs.python311;
+                uv.enable = true;
+              };
+            }
+          ];
+        };
+      });
+  };
+}
